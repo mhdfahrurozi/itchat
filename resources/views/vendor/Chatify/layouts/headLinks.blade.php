7c69689b9765100c09faad1beb9db757e02af80f
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2025-08-23 21:33:14 +0700

    update

--- resources/views/vendor/Chatify/layouts/headLinks.blade.php
+++ resources/views/vendor/Chatify/layouts/headLinks.blade.php
@@ -7,6 +7,7 @@
 <meta name="messenger-color" content="{{ $messengerColor }}">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="url" content="{{ url('').'/'.config('chatify.routes.prefix') }}" data-user="{{ Auth::user()->id }}">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 <link rel="icon" type="image/x-icon" href="{{ asset('images') }}/logochat.jpg" />
 {{-- scripts --}}
 <script
